6c3b9368fe4580e1012cee2a2a02df0b46d785be
Melany Suarez <melanyjsuarez@gmail.com>
2023-04-27 08:16:38 -0500

    Crea listas, muestra su tamaño y crea .gitignore

---
+++ main.py
@@ -0,0 +1,27 @@
+edad = 12
+altura = 1.79
+nombre = "Daniel"
+
+lista1= [10,5,3,9]
+lista2= [1.79, 1.67, 1.82, 1.59]
+lista3 = ["Lunes", "Martes","Miércoles"]
+lista4 = ["Daniel", 12, 1.79]
+
+
+
+
+if __name__ == '__main__':
+    #Imprime la cantidad de elementos de las listas
+    print(len(lista1))
+    print(len(lista2))
+    print(len(lista3))
+    print(len(lista4))
+
+    print()
+    print(lista1)
+    lista1[0] = 1
+    print()
+    print(lista1)
+
+    print(lista1[3])
+
